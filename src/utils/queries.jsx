c4d0def8674c4cc3d93418e5274c5e79de485086
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2022-04-19 17:00:53 +0200

    Update queries.jsx

--- src/utils/queries.jsx
+++ src/utils/queries.jsx
@@ -20,3 +20,41 @@ export const GetAllUsersQuery = async () => {
 
   return fetch("http://localhost:3001/matchmaking/getAll", requestOptions);
 };
+
+
+export const AskMatch = async (matchmakingId) => {
+  const token = useSelector((state) => state.main.user.token);
+
+
+  const params = {
+    matchmakingId : matchmakingId,
+    };
+
+
+  const requestOptions = {
+    method: "GET",
+    headers: { "Content-Type": "application/json", "WWW-Authenticate": token },
+    body: JSON.stringify(params),
+  };
+
+  return fetch("http://localhost:3001/matchmaking/request", requestOptions);
+};
+
+
+export const AcceptRequestMatch = async (matchmakingId) => {
+  const token = useSelector((state) => state.main.user.token);
+
+
+  const params = {
+    matchmakingId : matchmakingId,
+    };
+
+
+  const requestOptions = {
+    method: "GET",
+    headers: { "Content-Type": "application/json", "WWW-Authenticate": token },
+    body: JSON.stringify(params),
+  };
+
+  return fetch("http://localhost:3001/matchmaking/acceptRequest", requestOptions);
+};
