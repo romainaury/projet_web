e1b739a5ac3b89afe362013ae28f880ad5909a04
Romain <romain.aury@hotmail.com>
2022-04-19 16:54:18 +0200

    added liste joueurs

---
+++ src/components/joueurs/ListeJoueursComponent.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./liste-joueurs-style.scss";
+
+const ListeJoueursComponent = ({ user }) => {
+  const [joueurs, setJoueurs] = useState([]);
+
+  useEffect([]);
+
+  return <div>ListeJoueursComponent</div>;
+};
+
+export default ListeJoueursComponent;
