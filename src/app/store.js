2a4bc570f007f2f1b221d1ae5ccc1c940a679738
Benjamin Creurer <70890541+NeoBlueIsReal@users.noreply.github.com>
2022-04-19 18:24:36 +0200

    added some match data WIP

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mainReducer from "../reducers/mainReducer";
+import matchReducer from "../reducers/matchReducer";
 
 export const store = configureStore({
   reducer: {
     main: mainReducer,
+    match: matchReducer,
   },
   devTools: true,
 });
---
+++ src/reducers/matchReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    status: "",
+    player1: {
+        hp: undefined,
+        hand: [],
+        deck: [],
+        cardPicked: undefined,
+        turn: false,
+    },
+    player2: {
+        hp: undefined,
+        hand: [],
+        deck: [],
+        cardPicked: undefined,
+        turn: false,
+    }
+}
